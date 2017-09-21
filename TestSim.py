681764af90df3326fb2e965ae23021450d6df830
JoseH1497 <jherrera52@ucmerced.edu>
2017-09-20 23:48:27 -0700

    floodingCommitment

--- TestSim.py
+++ TestSim.py
@@ -119,7 +119,7 @@ class TestSim:
         self.msg.set_dest(dest);
         self.msg.set_src(ID);
         self.msg.setString_payload(payloadStr)
-        self.msg.set_seq(0)
+        #self.msg.set_seq(0)
         self.msg.set_TTL(15)
         self.msg.set_protocol(99)
 
